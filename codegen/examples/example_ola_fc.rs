d71e8848dc28f817aa8bda0eb06f7ecf282f47b6
bchyl <bchylmn@gmail.com>
2023-02-12 10:09:30 +0800

    test ola backend codegen

---
+++ codegen/examples/example_ola_fc.rs
@@ -0,0 +1,71 @@
+use vicis_codegen::{self, isa::ola::Ola, lower::compile_module};
+use vicis_core::ir::module::Module;
+
+fn main() {
+    // LLVM Assembly
+    let asm = r#"
+  source_filename = "asm" 
+
+  ; Function Attrs: noinline nounwind optnone ssp uwtable
+  define i32 @bar(i32 %0, i32 %1) #0 {
+    %3 = alloca i32, align 4
+    %4 = alloca i32, align 4
+    %5 = alloca i32, align 4
+    store i32 %0, i32* %3, align 4
+    store i32 %1, i32* %4, align 4
+    store i32 200, i32* %5, align 4
+    %6 = load i32, i32* %3, align 4
+    %7 = load i32, i32* %4, align 4
+    %8 = add nsw i32 %6, %7
+    store i32 %8, i32* %5, align 4
+    %9 = load i32, i32* %5, align 4
+    ret i32 %9
+  }
+  
+  ; Function Attrs: noinline nounwind optnone ssp uwtable
+  define i32 @foo() #0 {
+    %1 = alloca i32, align 4
+    %2 = alloca i32, align 4
+    %3 = alloca i32, align 4
+    store i32 10, i32* %1, align 4
+    store i32 20, i32* %2, align 4
+    store i32 100, i32* %3, align 4
+    %4 = load i32, i32* %1, align 4
+    %5 = load i32, i32* %2, align 4
+    %6 = call i32 @bar(i32 %4, i32 %5)
+    store i32 %6, i32* %3, align 4
+    %7 = load i32, i32* %3, align 4
+    ret i32 %7
+  }  
+"#;
+
+    // Parse the assembly and get a module
+    let module = Module::try_from(asm).expect("failed to parse LLVM IR");
+
+    // Compile the module for x86 and get a machine module
+    let isa = Ola::default();
+    let mach_module = compile_module(&isa, &module).expect("failed to compile");
+    println!("{}",mach_module.display_asm());
+
+    // Display the machine module as assembly
+    /*assert_eq!(
+        format!("{}", mach_module.display_asm()),
+        "main:
+.LBL0_0:
+  add r8 r8 1
+  mstore [r8,-1] 10
+  mload r4 [r8,-1]
+  add r0 r4 20
+  add r1 r4 30
+  mul r2 r0 r1
+  not r6 r1
+  add r6 r6 1
+  add r3 r2 r6
+  mov r0 r3
+  not r6 1
+  add r6 r6 1
+  add r8 r8 r6
+  ret 
+"
+    );*/
+}
---
+++ codegen/src/isa/ola/pass/eliminate_slot.rs
@@ -0,0 +1,90 @@
+use crate::{
+    function::{Function, instruction::TargetInst},
+    isa::ola::{instruction::OperandData, register::GR, Ola},
+    module::Module,
+};
+use vicis_core::ir::types;
+use anyhow::Result;
+
+pub fn run_on_module(module: &mut Module<Ola>) -> Result<()> {
+    for (_, func) in &mut module.functions {
+        run_on_function(func);
+    }
+    Ok(())
+}
+
+pub fn run_on_function(function: &mut Function<Ola>) {
+    let mut worklist = vec![];
+    let mut call = false;
+
+    for block in function.layout.block_iter() {
+        for inst_id in function.layout.inst_iter(block) {
+            let inst = function.data.inst_ref(inst_id);
+            if inst
+                .data
+                .operands
+                .iter()
+                .any(|op| matches!(op.data, OperandData::Slot(_)))
+            {
+                worklist.push(inst_id);
+            }
+            if inst.data.is_call() {
+                call = true;
+            }
+        }
+    }
+
+    if call {
+        for _ in 0..1 {
+            function.slots.add_slot(types::I32,1,1);
+        }
+    }
+    function.slots.ensure_computed_offsets();
+
+    while let Some(inst_id) = worklist.pop() {
+        let mut inst = function.data.instructions[inst_id].clone();
+
+        let mut i = 0;
+        let len = inst.data.operands.len();
+
+        while i < len {
+            // MemStart indicates the beginning of memory arguments
+            if !matches!(&inst.data.operands[i].data, &OperandData::MemStart) {
+                i += 1;
+                continue;
+            }
+
+            i += 1;
+
+            let mem = &mut inst.data.operands[i..i + 6];
+
+            match (&mem[1].data, &mem[2].data) {
+                (OperandData::Slot(slot), OperandData::None) => {
+                    let off = function.slots.get(*slot).offset;
+                    mem[1].data = OperandData::None;
+                    let mut size = off as i32 / 4  - function.slots.arena.len() as i32;
+                    if call {
+                        size -= 1;
+                    }
+                    mem[2].data = OperandData::Int32(size);
+                    mem[3].data = OperandData::Reg(GR::R8.into());
+                }
+                (OperandData::Slot(slot), OperandData::Int32(imm)) => {
+                    let off = function.slots.get(*slot).offset;
+                    let mut size = function.slots.arena.len() as i32 + (*imm - off as i32) / 4;
+                    if call {
+                        size -=1;
+                    }
+                    mem[2].data = OperandData::Int32(size);
+                    mem[1].data = OperandData::None;
+                    mem[3].data = OperandData::Reg(GR::R8.into());
+                }
+                _ => todo!(),
+            }
+
+            i += 6;
+        }
+
+        function.data.instructions[inst_id] = inst;
+    }
+}
